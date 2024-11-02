89452a65c0ed5296ebe1cd9992e85c5042f419dc
ChiriacPetrica <brink847@gmail.com>
2024-11-02 13:28:42 +0200

    Background rezolutie full

--- src/ui/LandingPage.jsx
+++ src/ui/LandingPage.jsx
@@ -4,7 +4,7 @@ import Hero from "../pages/Hero";
 
 const LandingPage = () => {
   return (
-    <div className="flex min-h-screen flex-col bg-[url('/src/assets/bg.jpg')] bg-cover bg-center">
+    <div className="flex min-h-screen flex-col bg-[url('/src/assets/bg.png')] bg-cover bg-center">
       <Navbar />
       <Hero />
       <Footer />
